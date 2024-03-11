638263d04d8feefbc63617a4c7cec4d7b5f983de
Ivan Muratov <binakot@gmail.com>
2024-03-11 09:50:49 +0300

    Fix CORS

--- src/main/java/ru/kubsu/geocoder/controller/GeocoderController.java
+++ src/main/java/ru/kubsu/geocoder/controller/GeocoderController.java
@@ -3,6 +3,7 @@ package ru.kubsu.geocoder.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,6 +28,7 @@ public class GeocoderController {
     }
 
     @GetMapping(value = "/search", produces = APPLICATION_JSON_VALUE)
+    @CrossOrigin(origins = "*")
     public ResponseEntity<Address> search(final @RequestParam String query) {
         return addressService.search(query)
             .map(p -> ResponseEntity.status(HttpStatus.OK).body(p))
